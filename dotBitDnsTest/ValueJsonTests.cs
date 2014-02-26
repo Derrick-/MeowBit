e2b477be852d2c28fc94f34938196af1452b1f73
Derrick- <derrick@alienseed.com>
2014-02-26 03:01:23 +0000

    Expanded NameShowResponse to handle email, maps, infos and translate Added tests for NameShowResponse
    git-svn-id: https://svn.alienseed.com:8443/svn/alienseed/trunk/dotBitNS@2698 4bf54c5a-0cb7-ff49-8398-c775ed83c514

---
+++ dotBitDnsTest/ValueJsonTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NamecoinLib.Responses;
+using System.Linq;
+
+namespace dotBitDnsTest
+{
+    [TestClass]
+    public class ValueJsonTests
+    {
+        [TestMethod]
+        public void JsonParseTest()
+        {
+            string email = "gtempns1@san.gs";
+            string ip = "78.47.86.43";
+            string ipWWW = "78.47.86.44";
+            string Json1 = "{\"ip\": \"" + ip + "\", \"email\": \"" + email + "\", \"info\": { \"status\": \"On sale.\" }, \"map\": {\"\": \"" + ip + "\", \"www\": \"" + ipWWW + "\"}} ";
+
+            NameShowResponse Response1 = new NameShowResponse()
+            {
+                address = "Json1.bit",
+                value = Json1
+            };
+
+            var result = Response1.GetValue();
+
+            Assert.AreEqual(ip, result.ip);
+            Assert.AreEqual(email, result.email);
+
+            var mapped = result.Maps;
+            Assert.IsTrue(mapped.Contains(""));
+            Assert.IsTrue(mapped.Contains("www"));
+
+            Assert.AreEqual(ip, result.GetMapIps("").First());
+            Assert.AreEqual(ipWWW, result.GetMapIps("www").First());
+
+            Assert.AreEqual("On sale.", result.GetInfoValue("status").First());
+        }
+
+        [TestMethod]
+        public void TranslateParseTest()
+        {
+            string Json1 = "{\"translate\": \"bitcoin.org\", \"ns\": [\"1.2.3.4\", \"1.2.3.5\"]}";
+
+            NameShowResponse Response1 = new NameShowResponse()
+            {
+                address = "Json1.bit",
+                value = Json1
+            };
+
+            var result = Response1.GetValue();
+            Assert.IsNull(result.ip);
+
+            Assert.AreEqual("bitcoin.org", result.translate);
+            Assert.IsTrue(result.ns.Contains("1.2.3.4"));
+            Assert.IsTrue(result.ns.Contains("1.2.3.5"));
+        }
+    }
+}
--- dotBitNS/NamecoinClient/Responses/NameShowResponse.cs
+++ dotBitNS/NamecoinClient/Responses/NameShowResponse.cs
@@ -1,6 +1,11 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
+using Newtonsoft.Json.Linq;
+using System.Linq;
 
 namespace NamecoinLib.Responses
 {
@@ -26,14 +31,44 @@ namespace NamecoinLib.Responses
 
     }
 
+    // {"translate": "bitcoin.org", "ns": ["1.2.3.4", "1.2.3.5"]} 
+    // {"ip": "78.47.86.43", "email": "gtempns1@san.gs", "info": { "status": "On sale." }, "map": {"": "78.47.86.43", "www": "78.47.86.43"}}
+    // {"ip": "78.47.86.43", "map": {"": "78.47.86.43", "www": "78.47.86.43"}}
+
     public class NameValue
     {
         public string ip { get; set; }
-        public address map { get; set; }
+        public string email { get; set; }
+        public JObject info { get; set; }
+        public JObject map { get; set; }
+        public string translate { get; set; }
+        public string[] ns { get; set; } 
 
-        public class address
+        public IEnumerable<string> Maps
         {
-            public string ip { get; set; }
+            get
+            {
+                return map.Properties().Select(m => m.Name);
+            }
+        }
+
+        public IEnumerable<string> Infos
+        {
+            get
+            {
+                return info.Properties().Select(m => m.Name);
+            }
+        }
+
+        public IEnumerable<string> GetMapIps(string name)
+        {
+            return map.Properties().Where(m => m.Name == name && m.Value.Type == JTokenType.String).Select(m => (string)m.Value);
+        }
+
+
+        public IEnumerable<string> GetInfoValue(string name)
+        {
+            return info.Properties().Where(m => m.Name == name && m.Value.Type == JTokenType.String).Select(m => (string)m.Value);
         }
     }
 }
