4b41773f8a0dbd434c68e99e1589913de5dd5ebd
Derrick- <derrick@alienseed.com>
2014-03-08 18:07:08 +0000

    Rearrange Logging Settings items Show open folder as link instead of button
    git-svn-id: https://svn.alienseed.com:8443/svn/alienseed/trunk/dotBitNS@2744 4bf54c5a-0cb7-ff49-8398-c775ed83c514

---
+++ dotBitNs Monitor/Converters/VisibilityConverters.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace dotBitNs_Monitor.Converters
+{
+    public class NullToVisibiltyConverter : IValueConverter
+    {
+        public virtual object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value == null) ? Visibility.Collapsed : Visibility.Visible;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        { return null; }
+    }
+
+    public class InverseNullToVisibiltyConverter : NullToVisibiltyConverter
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value == null) ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+
+}
